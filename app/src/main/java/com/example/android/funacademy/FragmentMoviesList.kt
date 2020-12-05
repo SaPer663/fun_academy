ff9ec08fa922a5368d1abb1fb26cde7869737253
saper663_home <saper663ru@gmail.com>
2020-12-05 17:42:51 +0500

    inflated fragments in the activity and added a listener to CardView

---
+++ app/src/main/java/com/example/android/funacademy/FragmentMoviesList.kt
@@ -0,0 +1,48 @@
+package com.example.android.funacademy
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.cardview.widget.CardView
+import androidx.fragment.app.Fragment
+
+class FragmentMoviesList : Fragment() {
+
+    private var listener: ClickListener? = null
+    private var cardView: CardView? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = inflater.inflate(R.layout.fragment_movies_list, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        cardView = view.findViewById<CardView>(R.id.card_view).apply {
+            setOnClickListener { listener?.clickFragment() }
+        }
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is ClickListener) {
+            listener = context
+        }
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        listener = null
+    }
+
+    fun setListener(l: ClickListener) {
+        listener = l
+    }
+
+    interface ClickListener {
+        fun clickFragment()
+    }
+}
