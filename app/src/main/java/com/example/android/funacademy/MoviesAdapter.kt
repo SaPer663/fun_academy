a2123d1319de7b31833b5b315f7bd84403ac1458
saper663_home <saper663ru@gmail.com>
2021-01-05 12:07:12 +0500

    add MoviesAdapter

--- app/src/main/java/com/example/android/funacademy/MoviesAdapter.kt
+++ app/src/main/java/com/example/android/funacademy/MoviesAdapter.kt
@@ -2,19 +2,21 @@ package com.example.android.funacademy
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.RatingBar
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.example.android.funacademy.databinding.ViewHolderMovieBinding
+import com.example.android.funacademy.databinding.ViewMovieItemBinding
 import com.example.android.funacademy.models.Movie
 
-class MoviesAdapter : RecyclerView.Adapter<MovieViewHolder>() {
+class MoviesAdapter(private val clickListener: Listener) : RecyclerView.Adapter<MovieViewHolder>() {
 
-    private lateinit var movies: List<Movie>
+    private var movies: List<Movie> = listOf()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
-        val view = ViewHolderMovieBinding
+        val view = ViewMovieItemBinding
             .inflate(LayoutInflater.from(parent.context), parent, false)
-        return MovieViewHolder(view)
+        return MovieViewHolder(view, clickListener, movies)
     }
 
     override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
@@ -28,13 +30,51 @@ class MoviesAdapter : RecyclerView.Adapter<MovieViewHolder>() {
     fun bindMovies(newMovies: List<Movie>) {
         movies = newMovies
     }
+
 }
-class MovieViewHolder(viewHolderMovieBinding: ViewHolderMovieBinding) :
+
+interface Listener {
+    fun clickFragment(id: Int)
+}
+
+class MovieViewHolder(
+    viewHolderMovieBinding: ViewMovieItemBinding,
+    clickListener: Listener,
+    movies: List<Movie>
+) :
     RecyclerView.ViewHolder(viewHolderMovieBinding.root) {
 
-    private val tag: TextView = viewHolderMovieBinding.frTag
-    fun onBind(movie: Movie) {
-        tag.setText(movie.genre)
+    private val name: TextView = viewHolderMovieBinding.nameMovie
+    private val genre: TextView = viewHolderMovieBinding.genre
+    private val bgImageMovie: ImageView = viewHolderMovieBinding.bgImageMovie
+    private val ratingBar: RatingBar = viewHolderMovieBinding.frRatingbar
+    private val favorites: ImageView = viewHolderMovieBinding.favorites
+    private val reviews: TextView = viewHolderMovieBinding.frTextReviews
+    private val durationMin: TextView = viewHolderMovieBinding.duration
+    private val ageRating: TextView = viewHolderMovieBinding.ageRating
+
+    init {
+        viewHolderMovieBinding.root.setOnClickListener {
+            val position = adapterPosition
+            if (position != RecyclerView.NO_POSITION) {
+                clickListener.clickFragment(movies[position].id)
+            }
+        }
     }
 
+
+    fun onBind(movie: Movie) {
+
+        name.text = movie.name
+        genre.setText(movie.genre)
+        bgImageMovie.setImageResource(movie.picForList)
+        ratingBar.rating = movie.Rating
+        "${movie.numOfRatings} Reviews".also { reviews.text = it }
+        "${movie.durationMin}min".also { durationMin.text = it }
+        ageRating.text = movie.ageRating
+        if (movie.favorite) {
+            favorites.setImageResource(R.color.pink)
+        }
     }
+
+}
