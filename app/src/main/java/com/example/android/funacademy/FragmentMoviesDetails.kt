a8ed150d84bb9f430553a10bdf421eedc36f0c76
saper663_home <saper663ru@gmail.com>
2021-01-05 12:13:38 +0500

    change FragmentMoviesDetails.kt

--- app/src/main/java/com/example/android/funacademy/FragmentMoviesDetails.kt
+++ app/src/main/java/com/example/android/funacademy/FragmentMoviesDetails.kt
@@ -1,16 +1,15 @@
 package com.example.android.funacademy
 
-
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.widget.TextView
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.android.funacademy.databinding.FragmentMoviesDetailsBinding
 import com.example.android.funacademy.domain.MoviesDataSource
+import com.example.android.funacademy.models.Movie
 
 class FragmentMoviesDetails : Fragment(R.layout.fragment_movies_details) {
 
@@ -24,11 +23,20 @@ class FragmentMoviesDetails : Fragment(R.layout.fragment_movies_details) {
         val binding = FragmentMoviesDetailsBinding.bind(view)
         fragmentMoveDetailsBinding = binding
         val recycler: RecyclerView = binding.rvActors
-        recycler.layoutManager = LinearLayoutManager(requireContext())
+        recycler.layoutManager =
+            LinearLayoutManager(requireContext(), RecyclerView.HORIZONTAL, false)
         recycler.adapter = adapter
         tvBack = binding.back.apply {
             setOnClickListener { onClickBack() }
         }
+        val movie: Movie = MoviesDataSource().getMovieById(arguments?.getInt(ARGS_MOVIE))
+        binding.backgroundImage.setImageResource(movie.picForDetails)
+        binding.rectangl13.text = movie.ageRating
+        binding.name.text = movie.name
+        binding.tag.setText(movie.genre)
+        binding.ratingbar.rating = movie.Rating
+        "${movie.numOfRatings} Reviews".also { binding.textReviews.text = it }
+        binding.storylineName.setText(movie.storyline)
     }
 
     override fun onStart() {
@@ -37,7 +45,10 @@ class FragmentMoviesDetails : Fragment(R.layout.fragment_movies_details) {
     }
 
     private fun updateData() {
-        adapter.bindActors(MoviesDataSource().getMovies())
+        adapter.bindActors(
+            MoviesDataSource()
+                .getMovieById(this.arguments?.getInt(ARGS_MOVIE)).actors
+        )
         adapter.notifyDataSetChanged()
     }
 
@@ -49,6 +60,15 @@ class FragmentMoviesDetails : Fragment(R.layout.fragment_movies_details) {
     private fun onClickBack() {
         activity?.supportFragmentManager?.apply {
             popBackStack()
+        }
+    }
+
+    companion object {
+        private const val ARGS_MOVIE = "ARGS_MOVIE"
+        fun newInstance(id: Int): FragmentMoviesDetails {
+            return FragmentMoviesDetails().apply {
+                arguments = bundleOf(ARGS_MOVIE to id)
+            }
         }
     }
 
