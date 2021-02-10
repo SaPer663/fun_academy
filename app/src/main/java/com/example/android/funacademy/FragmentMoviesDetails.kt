8b6d5a346394a58b95df3ca62edeeb7c527fd0cf
saper663_home <saper663ru@gmail.com>
2021-02-10 13:01:58 +0500

    add setupView method

--- app/src/main/java/com/example/android/funacademy/FragmentMoviesDetails.kt
+++ app/src/main/java/com/example/android/funacademy/FragmentMoviesDetails.kt
@@ -1,5 +1,6 @@
 package com.example.android.funacademy
 
+import android.content.Context
 import android.os.Bundle
 import android.view.View
 import android.widget.TextView
@@ -8,14 +9,10 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import com.example.android.funacademy.data.getListGenre
 import com.example.android.funacademy.data.loadMovies
 import com.example.android.funacademy.databinding.FragmentMoviesDetailsBinding
 import com.example.android.funacademy.model.Movie
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.*
 
 class FragmentMoviesDetails : Fragment(R.layout.fragment_movies_details) {
 
@@ -24,27 +21,12 @@ class FragmentMoviesDetails : Fragment(R.layout.fragment_movies_details) {
     private val scope = CoroutineScope(Dispatchers.Main)
     private var fragmentMoveDetailsBinding: FragmentMoviesDetailsBinding? = null
     private val binding get() = fragmentMoveDetailsBinding!!
-    private var cacheMovie: Movie? = null
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         fragmentMoveDetailsBinding = FragmentMoviesDetailsBinding.bind(view)
         scope.launch {
-            cacheMovie = obtainMovie()
-            cacheMovie?.let { fillViews(it) }
-            if (cacheMovie?.actors?.isEmpty() == true) {
-                binding.apply {
-                    castName.visibility = View.GONE
-                    rvActors.visibility = View.GONE
-                }
-            } else {
-                actorsAdapter = ActorsAdapter(cacheMovie?.actors)
-                binding.rvActors.apply {
-                    layoutManager =
-                        LinearLayoutManager(requireContext(), RecyclerView.HORIZONTAL, false)
-                    adapter = actorsAdapter
-                }
-            }
+            setupView()
         }
         tvBack = binding.back.apply {
             setOnClickListener { onClickBack() }
@@ -53,6 +35,7 @@ class FragmentMoviesDetails : Fragment(R.layout.fragment_movies_details) {
 
     override fun onDestroyView() {
         fragmentMoveDetailsBinding = null
+        scope.cancel()
         super.onDestroyView()
     }
 
@@ -65,22 +48,46 @@ class FragmentMoviesDetails : Fragment(R.layout.fragment_movies_details) {
     private fun fillViews(movie: Movie) {
         binding.apply {
             Glide
-                .with(requireContext())
+                .with(this@FragmentMoviesDetails)
                 .load(movie.imageUrl)
                 .into(backgroundImage)
             "${movie.pgAge}+".also { rectangl13.text = it }
             name.text = movie.title
-            tag.text = getListGenre(movie.genres).joinToString()
+            tag.text = movie.genres.joinToString { it.name }
             ratingbar.rating = (movie.rating / 10).toFloat()
             "${movie.reviewCount} Reviews".also { textReviews.text = it }
             storyline.text = movie.storyLine
+
         }
     }
 
-    private suspend fun obtainMovie() = withContext(Dispatchers.IO) {
+    private suspend fun setupView() {
+        val cacheMovie = obtainMovie(
+            requireContext(),
+            arguments?.getInt(ARGS_MOVIE) ?: 0
+        )
 
-        loadMovies(requireContext()).find { it.id == arguments?.getInt(ARGS_MOVIE) ?: 0 }
+        cacheMovie?.let {
+            fillViews(it)
+            if (!it.actors.isNullOrEmpty()) {
+                actorsAdapter = ActorsAdapter(it.actors)
+                binding.apply {
+                    castName.visibility = View.VISIBLE
+                    rvActors.visibility = View.VISIBLE
+                    rvActors.apply {
+                        layoutManager =
+                            LinearLayoutManager(
+                                requireContext(),
+                                RecyclerView.HORIZONTAL,
+                                false
+                            )
+                        adapter = actorsAdapter
+                    }
+                }
+            }
+        }
     }
+
 
     companion object {
         private const val ARGS_MOVIE = "ARGS_MOVIE"
@@ -91,4 +98,9 @@ class FragmentMoviesDetails : Fragment(R.layout.fragment_movies_details) {
         }
     }
 
+}
+
+suspend fun obtainMovie(context: Context, id: Int) = withContext(Dispatchers.IO) {
+
+    loadMovies(context).find { it.id == id }
 }
