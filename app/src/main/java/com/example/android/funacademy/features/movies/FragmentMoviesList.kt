658989733500cab212ce8e699fd553320882f1ac
saper663_home <saper663ru@gmail.com>
2021-02-27 12:42:40 +0500

    add viewModel and resourceProvider

--- app/src/main/java/com/example/android/funacademy/features/movies/FragmentMoviesList.kt
+++ app/src/main/java/com/example/android/funacademy/features/movies/FragmentMoviesList.kt
@@ -1,48 +1,49 @@
-package com.example.android.funacademy
+package com.example.android.funacademy.features.movies
 
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.commit
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.GridLayoutManager
-import com.example.android.funacademy.data.loadMovies
+import com.example.android.funacademy.R
 import com.example.android.funacademy.databinding.FragmentMoviesListBinding
-import com.example.android.funacademy.model.Movie
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.cancel
-import kotlinx.coroutines.launch
+import com.example.android.funacademy.features.detailmovie.FragmentMoviesDetails
+import com.example.android.funacademy.utils.ResourceProvider
 
 class FragmentMoviesList : Fragment(R.layout.fragment_movies_list), ListenerMoviesAdapter {
 
     private lateinit var moviesAdapter: MoviesAdapter
     private var fragmentMoviesListBinding: FragmentMoviesListBinding? = null
     private val binding get() = fragmentMoviesListBinding!!
-    private lateinit var scope: CoroutineScope
+    private lateinit var viewModel: ViewModelMoviesList
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         moviesAdapter = MoviesAdapter(this)
         fragmentMoviesListBinding = FragmentMoviesListBinding.bind(view)
+        val resourceProvider = ResourceProvider(requireContext())
+        val viewModelFactory = ViewModelMoviesListFactory(resourceProvider)
+        viewModel = ViewModelProvider(this, viewModelFactory).get(ViewModelMoviesList::class.java)
         binding.rvMovies.apply {
-            layoutManager = GridLayoutManager(requireContext(), 2)
+            layoutManager = GridLayoutManager(this.context, 2)
             adapter = moviesAdapter
         }
-        scope = CoroutineScope(Dispatchers.Main)
     }
 
     override fun onStart() {
         super.onStart()
-        scope.launch { context?.let { updateData(loadMovies(it)) } }
+        loadData()
     }
 
-    private fun updateData(data: List<Movie>) {
-        moviesAdapter.bindMovies(data)
+    private fun loadData() {
+        viewModel.movies.observe(viewLifecycleOwner, { movieList ->
+            moviesAdapter.bindMovies(movieList)
+        })
     }
 
     override fun onDestroyView() {
         fragmentMoviesListBinding = null
-        scope.cancel()
         super.onDestroyView()
     }
 
