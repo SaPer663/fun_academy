8e388086d41ada181ee59c41e065458b84dee8e9
saper663_home <saper663ru@gmail.com>
2021-02-27 12:32:52 +0500

    changed the path to the FragmentMoviesList

--- app/src/main/java/com/example/android/funacademy/MainActivity.kt
+++ app/src/main/java/com/example/android/funacademy/MainActivity.kt
@@ -1,10 +1,9 @@
 package com.example.android.funacademy
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.replace
+import com.example.android.funacademy.features.movies.FragmentMoviesList
 
 class MainActivity : AppCompatActivity() {
 
