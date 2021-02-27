47d749c85cf461bb23f3a380ea4bd8f835f683f0
saper663_home <saper663ru@gmail.com>
2021-02-27 12:39:25 +0500

    created ViewModelMoviesList

---
+++ app/src/main/java/com/example/android/funacademy/features/movies/ViewModelMoviesList.kt
@@ -0,0 +1,25 @@
+package com.example.android.funacademy.features.movies
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.android.funacademy.model.Movie
+import com.example.android.funacademy.utils.ResourceProvider
+import kotlinx.coroutines.launch
+
+class ViewModelMoviesList(val resources: ResourceProvider) : ViewModel() {
+
+    private var _movies = MutableLiveData<List<Movie>>()
+    val movies: LiveData<List<Movie>> get() = _movies
+
+    init {
+        loadMovies()
+    }
+
+    private fun loadMovies() {
+        viewModelScope.launch {
+            _movies.postValue(resources.loadMovies())
+        }
+    }
+}
